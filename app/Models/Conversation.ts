80e96bd7694295cecd371bc7827ecdcd0d511a52
zakka <mzakka163@gmail.com>
2025-08-09 09:16:51 +0700

    fix model

--- app/Models/Conversation.ts
+++ app/Models/Conversation.ts
@@ -1,11 +1,15 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Message from './Message'
 
 export default class Conversation extends BaseModel {
+  public static table = 'conversations'
   @column({ isPrimary: true }) public id: number
-  @column({ columnName: 'sesion_id' }) public sesionId: string
-  @column({ columnName: 'messages_id' }) public messagesId: number | null
-  @column({ columnName: 'last_messages' }) public lastMessages: string | null
+  @column() public sesionId: string
+  @column() public lastMessages?: string | null
   @column.dateTime({ autoCreate: true }) public createdAt: DateTime
   @column.dateTime({ autoCreate: true, autoUpdate: true }) public updatedAt: DateTime
+  @hasMany(() => Message, {
+    foreignKey: 'conversationId',
+  }) public messages: HasMany<typeof Message>
 }
--- app/Models/Message.ts
+++ app/Models/Message.ts
@@ -1,10 +1,15 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
-
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import Conversation from './Conversation'
 export default class Message extends BaseModel {
+  public static table = 'messages'
   @column({ isPrimary: true }) public id: number
+  @column() public conversationId: number
   @column({ columnName: 'sender_type' }) public senderType: string
   @column() public message: string
   @column.dateTime({ autoCreate: true }) public createdAt: DateTime
   @column.dateTime({ autoCreate: true, autoUpdate: true }) public updatedAt: DateTime
+  @belongsTo(() => Conversation, {
+    foreignKey: 'conversationId',
+  }) public conversation: BelongsTo<typeof Conversation>
 }
